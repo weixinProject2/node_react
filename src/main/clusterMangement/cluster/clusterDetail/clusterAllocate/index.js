2e01efc215d790f82f641805901b6f81e1862f10
MSFee <1360023821@qq.com>
2019-10-10 10:04:25 +0800

    增加集群权限功能

--- src/main/clusterMangement/cluster/clusterDetail/clusterAllocate/index.js
+++ src/main/clusterMangement/cluster/clusterDetail/clusterAllocate/index.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Table, Icon } from 'antd';
+import axios from 'axios';
+
+import './index.css'
+
 const ClusterAllocate = (props) =>{
+    const columns = [
+        {
+          title: '用户名',
+          dataIndex: 'userName',
+          width:200,
+          render:(text,record)=>{
+              const role = record.projectRole;
+              return (
+                  <div>
+                        {text}
+                      {role === 'M'?<Icon onClick={handleAllocate} style={{cursor: 'pointer',float:'right'}} type="more" />:null} 
+                  </div>
+              )
+          }
+        },
+        {
+          title: '登录名',
+          dataIndex: 'loginName',
+          width:200,
+        },
+        {
+          title: '项目角色',
+          width:200,
+          dataIndex: 'projectRoleDesc',
+        },
+        {
+            title: '添加时间',
+            dataIndex: 'addTime',
+        },
+      ];
+    useEffect(()=>{
+        getData();
+    },[])
+    const [tableList,setTableList] = useState([]);
+    function handleAllocate(){
+        
+    }
+    function getData(){
+        axios.post('http://localhost:3000/cluster/getAllocationInfo').then(res=>{
+            const list = res.data.list;
+            setTableList(list);
+    })
+    }
     return (
         <div>
-            权限分配
+             <Table
+            columns={columns}
+            dataSource={tableList}
+            scroll={{y: 290 }} 
+            rowKey="id"
+            align='center'
+            >
+
+            </Table>
         </div>
     )
 }
--- src/main/clusterMangement/cluster/clusterList/index.js
+++ src/main/clusterMangement/cluster/clusterList/index.js
@@ -1,25 +1,77 @@
+/* eslint-disable default-case */
 /* eslint-disable array-callback-return */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect,useContext } from 'react';
+import React, { useState, useEffect,useContext,useReducer } from 'react';
 import {  } from 'react-router-dom'
 import { toJS } from 'mobx'
 import axios from 'axios'
-import { Menu,Input, Icon } from 'antd';
+import { Menu,Input, Icon,Popover,Button,Drawer,Popconfirm, message,List } from 'antd';
 
 import { MyContext } from '../clusterStore'
 import './index.css';
 const { SubMenu }  = Menu;
 const ClusterList= (props) =>{
+
     const store = toJS(useContext(MyContext));
     const [rootSubmenuKeys,setRootSubmenuKeys] = useState([]);
     const [openkeys,setOpenKeys] = useState(['1']);
     const [nodeList,setNodeList] = useState([]);
+    const [stateDrawer,setStateDrawer] = useState(false);
+    const [clusterId,setclusterId] = useState('');
+    const init = {
+            title:'',
+            clusterId:'',
+            nodeList:[],
+    }
+    const [state, dispatch] = useReducer(reducer, init);
+    function reducer(state, action) {
+        switch (action.type){
+            case 'SETDATA':
+                return {
+                    ...action.payload
+                }
+            default:
+                return state;
+        }
+    }
+    const content = (
+        <div>
+          <Button onClick={modifyCluster}>修改集群</Button>
+          <br></br>
+          <Popconfirm
+            title="是否确定删除该集群?"
+            onConfirm={confirm}
+            okText="是"
+            cancelText="否"
+          >
+          <Button onClick={deleteCluster}>删除集群</Button>
+          </Popconfirm>
+        </div>
+      );
     useEffect(()=>{
         clusterNodeList();
     },[])
+    const showDrawer = () => {
+        setStateDrawer(true);
+      };
+    const onClose = () => {
+        setStateDrawer(false);
+      };
+      function confirm(e) {
+        message.success('删除成功');
+      }
+      
+    function modifyCluster(){
+        showDrawer();
+    }
+    function deleteCluster(){
+        // showModal();
+        console.log(state);
+    }
     function clusterNodeList(){
         axios.post('http://localhost:3000/cluster/getNodeList').then(res=>{
             const data = res.data.data
+            console.log(data);
             setNodeList(data);
             setrootSubmenuKeys(data);
             store.setCluster(data[0]);
@@ -44,7 +96,25 @@ const ClusterList= (props) =>{
         store.setCluster(item);
         const path = props.props.match.path;
         const title = item.children[0].sub
+        const reducerState = { title: item.title, clusterId:item.clusterId,nodeList:item.children }
+        setclusterId(item.clusterId);
+        dispatch({ type:'SETDATA',payload:reducerState })
         props.props.history.push(`${path}/clusterDeatil/`+`${title}`)
+    }
+    function hanldeDeleteNode(id){
+        axios.post('http://localhost:3000/cluster/clusterDeleteNode',{nodeId:id}).then(() => {
+                clusterNodeList();
+                console.log(nodeList);
+                nodeList.map(item=>{
+                    if (item.clusterId === clusterId) {
+                        const reducerState = { title: item.title, clusterId:item.clusterId,nodeList:item.children }
+                        console.log(reducerState);
+                        dispatch({ type:'SETDATA',payload:reducerState })
+                    }
+                })
+                message.success('删除成功');
+        })
+       
     }
     return (
         <div className="clusterList">
@@ -66,6 +136,9 @@ const ClusterList= (props) =>{
                         title={
                             <span>
                              <span>{item.title}</span>
+                             <Popover placement="bottom" content={content} trigger='click'>
+                                 <Icon style={{cursor: 'pointer', position: 'absolute',top: '30%',right:'10px'}} type="more" />
+                            </Popover>,
                             </span>
                         }
                         onTitleClick={()=>subMenuClick({...item})}
@@ -82,6 +155,33 @@ const ClusterList= (props) =>{
                   })
               }
             </Menu>
+            <Drawer
+                title={`修改集群 ------------ ${state.title}`}
+                placement="right"
+                width='500'
+                onClose={onClose}
+                closable={false}
+                visible={stateDrawer}
+                >
+                <List
+                    header={<div>该集群下的所有节点</div>}
+                    bordered
+                     dataSource={state.nodeList}
+                    renderItem={item => 
+                    <List.Item>
+                        <div style={{display:'flex',justifyContent:'space-between',width:'100%'}}>
+                            <span>节点名：{item.title}</span>
+                            <Icon onClick={()=>hanldeDeleteNode(item.nodeId)} type="close" style={{cursor:'pointer'}}/>
+                        </div>
+                    </List.Item>}
+                />
+                <div style={{color:'rgb(255,142,0)',cursor:'pointer',marginTop:'20px'}}>
+                <Icon type="plus" />
+                添加节点</div>
+                <Button style={{ position:'absolute',bottom:'10px',left:'30px'}} type='primary'>确定</Button>
+                <Button style={{ position:'absolute',bottom:'10px',left:'100px' }}>取消</Button>
+            </Drawer>
+        
         </div>
     )
 }
